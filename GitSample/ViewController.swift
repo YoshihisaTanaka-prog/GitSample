842a238db27830694b016530cd6a57efde318a37
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2020-12-10 13:06:58 +0900

    1st commit

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by 田中義久 on 2020/12/10.
+//  Copyright © 2020 Tanaka_Yoshihisa_4413. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
